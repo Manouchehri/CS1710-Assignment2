2dcd93e1b6a91900892bcec0fedb4f4fb2948095
David Manouchehri <david@davidmanouchehri.com>
2015-06-17 18:51:45 -0400

    Passes tests, complete.

--- PartIII/src/BarEntrance.java
+++ PartIII/src/BarEntrance.java
@@ -6,8 +6,26 @@
  *
  *         Page 194, Business P4.31
  */
+
+import java.util.Scanner;
+
 public class BarEntrance {
         public static void main(String[] args) {
+            Scanner userInput = new Scanner(System.in);
+            final int maxCap = 100;
 
+            System.out.println("Enter the number of people arriving or leaving.");
+
+            for(int currentCap = 0; userInput.hasNextInt();) {
+                int attemptCap = currentCap + userInput.nextInt();
+                if(attemptCap > maxCap) {
+                    System.out.println("Oyster bar is at capacity.");
+                    break; /* This could be removed to allow people to leave. */
+                }
+                else {
+                    System.out.println("Current capacity: " + attemptCap);
+                    currentCap = attemptCap;
+                }
+            }
         }
 }
