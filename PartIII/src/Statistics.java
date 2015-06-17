ea19e4c28d12df1c0d0cd6679a489e7394048aec
David Manouchehri <david@davidmanouchehri.com>
2015-06-17 18:26:56 -0400

    Passes test, looks complete.

--- PartIII/src/Statistics.java
+++ PartIII/src/Statistics.java
@@ -6,8 +6,26 @@
  *
  *         Page 190, Question P4.15
  */
+
+import java.util.Scanner;
+
 public class Statistics {
     public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Please set a set of floats. When you are done, enter any character to quit (e.g., \"h\" followed by the enter key.)");
 
+        double sum = 0, sumSquares = 0;
+        int count;
+        for(count = 0; userInput.hasNextFloat(); count++) {
+            double num = userInput.nextFloat();
+            sum += num;
+            sumSquares += Math.pow(num, 2);
+        }
+
+        double standardDeviation = Math.sqrt( (sumSquares - (Math.pow(sum, 2) / count) ) / (count - 1) );
+
+        System.out.println("Count of values: " + count);
+        System.out.println("Average: " + sum / count);
+        System.out.println("Standard deviation: " + standardDeviation);
     }
 }
