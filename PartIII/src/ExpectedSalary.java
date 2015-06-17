6dc0b36d874ef69d9f072d8325a0e6c5e1635b0d
David Manouchehri <david@davidmanouchehri.com>
2015-06-17 18:58:53 -0400

    Checked in shell.

---
+++ PartIII/src/ExpectedSalary.java
@@ -0,0 +1,14 @@
+/**
+ * @author David Manouchehri
+ *         Created on 6/17/15 at 6:56 PM.
+ *         All content is under the MIT License unless otherwise specified.
+ *         See LICENSE.txt for details.
+ */
+
+import java.util.Scanner;
+
+public class ExpectedSalary {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+    }
+}
