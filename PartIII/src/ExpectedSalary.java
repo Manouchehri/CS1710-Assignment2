c51cfba313322be5081316beef91037c94000b0e
David Manouchehri <david@davidmanouchehri.com>
2015-06-17 20:00:00 -0400

    Added input handler.

--- PartIII/src/ExpectedSalary.java
+++ PartIII/src/ExpectedSalary.java
@@ -10,5 +10,32 @@ import java.util.Scanner;
 public class ExpectedSalary {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
+
+        final int baseBSc = 28000;
+        final int expBSc = 6000;
+
+        final int baseMSc = 35000;
+        final int expMSc = 5000;
+
+        final int basePhD = 45000;
+        final int expPhD = 15000;
+
+        String degrees[] = {"BSc", "MSc", "PhD"};
+
+        /* Collect all the user input. */
+        String userInputs = "";
+        for(; userInput.hasNext();) {
+            String input = userInput.next();
+            if(input.equalsIgnoreCase("done"))
+                break;
+            userInputs += input;
+        }
+
+        Scanner scanner = new Scanner(userInputs);
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            System.out.println(line);
+        }
+        scanner.close();
     }
 }
