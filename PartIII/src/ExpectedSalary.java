5d889811aa5c3965ecf96d5f51f3e3b09fbf3c8c
David Manouchehri <david@davidmanouchehri.com>
2015-06-17 20:54:16 -0400

    Not that great, but functional.

--- PartIII/src/ExpectedSalary.java
+++ PartIII/src/ExpectedSalary.java
@@ -12,30 +12,78 @@ public class ExpectedSalary {
         Scanner userInput = new Scanner(System.in);
 
         final int baseBSc = 28000;
-        final int expBSc = 6000;
+        final int expBSc = 34000;
 
         final int baseMSc = 35000;
-        final int expMSc = 5000;
+        final int expMSc = 40000;
 
         final int basePhD = 45000;
-        final int expPhD = 15000;
+        final int expPhD = 60000;
 
-        String degrees[] = {"BSc", "MSc", "PhD"};
+        final String degrees[] = {"BSC", "MSC", "PHD"};
 
         /* Collect all the user input. */
         String userInputs = "";
         for(; userInput.hasNext();) {
             String input = userInput.next();
-            if(input.equalsIgnoreCase("done"))
+            if(input.equalsIgnoreCase("done")) {
+                userInputs += "\n";
                 break;
+            }
             userInputs += input + "\n";
         }
+        userInput.close();
 
-        Scanner scanner = new Scanner(userInputs);
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            System.out.println(line);
+        String ID = "";
+        int degree = 0, salary = 0;
+
+        Scanner liner = new Scanner(userInputs);
+        for (int i = 0; liner.hasNextLine(); i++) {
+            String line = liner.nextLine().toUpperCase();
+
+            if(i > 2) { /* If three lines have been read. */
+                i = 0;
+                System.out.printf("ID: %s + Expected Salary: $%.2f\n", ID, (double)salary);
+            }
+
+            if(i == 0)
+                ID = line;
+            else if(i == 1) {
+                if(line.equalsIgnoreCase(degrees[0]))
+                    degree = 0;
+                else if(line.equalsIgnoreCase(degrees[1]))
+                    degree = 1;
+                else if(line.equalsIgnoreCase(degrees[2]))
+                    degree = 2;
+                else {
+                    System.out.println("Unexpected input. Quitting.");
+                    break;
+                }
+            }
+            else if (i == 2)
+                if(Integer.parseInt(line) < 5)
+                    switch(degree) {
+                        case 0:
+                            salary = baseBSc;
+                            break;
+                        case 1:
+                            salary = baseMSc;
+                            break;
+                        case 2:
+                            salary = basePhD;
+                    }
+                else
+                    switch(degree) {
+                        case 0:
+                            salary = expBSc;
+                            break;
+                        case 1:
+                            salary = expMSc;
+                            break;
+                        case 2:
+                            salary = expPhD;
+                    }
         }
-        scanner.close();
+        liner.close();
     }
 }
